d9f054db572e181d6af90a3228a612a966ac4f46
Ayush Gajurel <49242566+Ayushgjl@users.noreply.github.com>
2020-01-22 21:52:08 +0545

    url and loginbll code

--- app/src/main/java/com/ayushgjl/vehicleservicing/API/UserAPI.java
+++ app/src/main/java/com/ayushgjl/vehicleservicing/API/UserAPI.java
@@ -1,5 +1,18 @@
 package com.ayushgjl.vehicleservicing.API;
 
+import com.ayushgjl.vehicleservicing.serverresponse.SignUpResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface UserAPI {
+//    @POST("users/signup")
+//    Call<SignUpResponse> registerUser(@Body User users);
+
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("email") String email, @Field("password") String password);
 
 }
