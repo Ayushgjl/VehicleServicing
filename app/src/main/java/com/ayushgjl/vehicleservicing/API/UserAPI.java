6df4647b0880980977203653500c137cf34c892c
Ayush Gajurel <49242566+Ayushgjl@users.noreply.github.com>
2020-01-26 21:27:20 +0545

    UI changes

--- app/src/main/java/com/ayushgjl/vehicleservicing/API/UserAPI.java
+++ app/src/main/java/com/ayushgjl/vehicleservicing/API/UserAPI.java
@@ -3,6 +3,7 @@ package com.ayushgjl.vehicleservicing.API;
 import com.ayushgjl.vehicleservicing.serverresponse.SignUpResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
