4bd83a02455dae654111175597d6dc9421c59175
Ayush Gajurel <49242566+Ayushgjl@users.noreply.github.com>
2020-01-26 23:07:30 +0545

    animated splash created

--- app/src/main/java/com/ayushgjl/vehicleservicing/SplashActivity.java
+++ app/src/main/java/com/ayushgjl/vehicleservicing/SplashActivity.java
@@ -2,26 +2,56 @@ package com.ayushgjl.vehicleservicing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import static java.lang.Thread.sleep;
+
 
 public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        imageView=(ImageView) findViewById(R.id.imageView);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        Animation  animation= AnimationUtils.loadAnimation(this,R.anim.myanimation);
+        imageView.startAnimation(animation);
+
+        Thread myThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent( SplashActivity.this,LoginActivity.class );
-                startActivity(intent);
-                finish();
+                try {
+                    sleep(4000);
+
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e){ e.printStackTrace();}
+
             }
-        }, 2000);
+        });
+        myThread.start();
+
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },1000);
+
     }
 }
