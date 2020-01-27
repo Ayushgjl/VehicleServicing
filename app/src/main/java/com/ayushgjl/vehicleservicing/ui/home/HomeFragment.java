db38919430027301f10b49bb1f8196b31ad1f660
Ayush Gajurel <49242566+Ayushgjl@users.noreply.github.com>
2020-01-27 20:57:01 +0545

    Navigation drawer and dashboard slider code and design

---
+++ app/src/main/java/com/ayushgjl/vehicleservicing/ui/home/HomeFragment.java
@@ -0,0 +1,48 @@
+package com.ayushgjl.vehicleservicing.ui.home;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.ayushgjl.vehicleservicing.R;
+import com.ayushgjl.vehicleservicing.ui.home.Adapter.ViewPagerAdapter;
+
+public class HomeFragment extends Fragment {
+    ViewPager viewPager;
+    private int position;
+    private static final int PAGE_NUM=4;
+
+    private Handler handler=new Handler();
+    private Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            viewPager.setCurrentItem(position,true);
+            if(position>=PAGE_NUM) position=0;
+            else position++;
+            handler.postDelayed(runnable,3000);
+        }
+    };
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        viewPager = view.findViewById(R.id.viewpager);
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getActivity());
+        viewPager.setAdapter(viewPagerAdapter);
+        runnable.run();
+        return view;
+
+    }
+}
